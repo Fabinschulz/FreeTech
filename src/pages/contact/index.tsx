ff3ba9ecc0ba5dc941cbf17cb8b397db1dc46b22
Fabio <fabio.lima19997@gmail.com>
2023-06-30 22:03:10 -0300

    fix reexport default schema file

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,12 +1,48 @@
 'use client';
 import { FieldValues, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { validationSchemaContactForm, defaultValuesContactForm } from './schema';
 import TextFormField from '@/components/TextFormField';
 import { env } from '@/config';
 import axiosApi from '@/api';
 import Toast from '@/components/Toast';
 import { useState } from 'react';
+import { object, string } from 'yup';
+
+function validationSchemaContactForm() {
+    return object().shape({
+        firstName: string()
+            .required('Nome é obrigatório')
+            .matches(/^[a-zA-Z]+$/, 'Nome inválido'),
+        lastName: string()
+            .required('Sobrenome é obrigatório')
+            .matches(/^[a-zA-Z]+$/, 'Sobrenome inválido'),
+        email: string()
+            .email('E-mail inválido')
+            .required('Email é obrigatório')
+            .test('email', 'Email inválido', (value) => {
+                if (value) {
+                    const email = value as string;
+                    const emailRegex = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$');
+
+                    return emailRegex.test(email);
+                }
+
+                return false;
+            }),
+        message: string()
+            .required('Mensagem é obrigatória')
+            .min(10, 'Mensagem deve ter no mínimo 10 caracteres'),
+        phoneNumber: string().required('Telefone é obrigatório')
+    });
+}
+
+const defaultValuesContactForm = {
+    firstName: null,
+    lastName: null,
+    email: null,
+    message: null,
+    phoneNumber: ''
+};
 
 function ContactPage() {
     const {
